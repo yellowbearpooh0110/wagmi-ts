5b734674eed1748b9a756aeeb5cd3b4382c408c9
YellowBearPooh <yellowbearpooh0110@gmail.com>
2022-08-04 22:47:28 +0900

    contract update

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const contractAddress = '0x70e98Ef4d981DD1abb6B1EF1487019A374EeA0ef';
+export const contractAddress = '0xc79B574Dc5Ecc008799cAE0467f8a4116b59E09e';
 
 export const networkId = 4;
 export const networkIdHex = '0x4';
--- src/contracts/WagmiClub.sol
+++ src/contracts/WagmiClub.sol
@@ -8,13 +8,7 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract WagmiClub is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    Pausable,
-    Ownable
-{
+contract WagmiClub is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
     using Strings for uint256;
     using Counters for Counters.Counter;
 
@@ -23,7 +17,6 @@ contract WagmiClub is
     Counters.Counter private _tokenIdCounter;
 
     uint256[999] public ids;
-    uint256 private index;
 
     uint256 public totalSuperNova;
     uint256 public totalNova;
@@ -38,12 +31,21 @@ contract WagmiClub is
     uint256 public maxSupply = 999;
     bool public onlyWhitelisted = true;
 
-    address[] public whitelistSuperNova;
-    address[] public whitelistNova;
-    address[] public whitelistAurora;
-    address[] public whitelistGenesis;
+    mapping(address => bool) public whitelistSuperNova;
+    mapping(address => uint256) public amountSuperNova;
+    mapping(address => uint256) public totalMintSuperNova;
 
-    mapping(address => uint256) public addressMintedBalance;
+    mapping(address => bool) public whitelistNova;
+    mapping(address => uint256) public amountNova;
+    mapping(address => uint256) public totalMintNova;
+
+    mapping(address => bool) public whitelistAurora;
+    mapping(address => uint256) public amountAurora;
+    mapping(address => uint256) public totalMintAurora;
+
+    mapping(address => bool) public whitelistGenesis;
+    mapping(address => uint256) public amountGenesis;
+    mapping(address => uint256) public totalMintGenesis;
 
     constructor() ERC721("Wagmi Club", "WC") {}
 
@@ -53,125 +55,81 @@ contract WagmiClub is
 
     function mintSuperNova(uint256 mintAmount) public payable {
         uint256 supply = totalSupply();
-        require(supply + mintAmount <= maxSupply, "Exceeds Max Supply");
-        require(msg.value >= costSuperNova * mintAmount, "Insufficient Funds");
+        require (supply + mintAmount <= maxSupply, "Exceeds Max Supply");
+        require (msg.value >= costSuperNova * mintAmount, "Insufficient Funds");
 
-        setBaseURI(
-            "https://gateway.pinata.cloud/ipfs/Qmb4CmubG1m1DWbScWrAoLQaVmcC4wY6BSU2bS6UXq6sgx/"
-        );
+        setBaseURI("https://gateway.pinata.cloud/ipfs/Qmb4CmubG1m1DWbScWrAoLQaVmcC4wY6BSU2bS6UXq6sgx/");
 
         if (msg.sender != owner()) {
             if (onlyWhitelisted == true) {
-                require(isSuperNova(msg.sender), "User Is Not Whitelisted");
-            }
+                require (whitelistSuperNova[msg.sender], "User Is Not Whitelisted");
+                require (totalMintSuperNova[msg.sender] < amountSuperNova[msg.sender], "Exceeded Whitelisted Amount!");
+            }   
         }
         uint256 tokenId = 0;
         totalSuperNova += mintAmount;
+        totalMintSuperNova[msg.sender] += mintAmount;
         _safeMint(msg.sender, tokenId + totalSuperNova);
-        delete whitelistSuperNova;
     }
 
     function mintNova(uint256 mintAmount) public payable {
         uint256 supply = totalSupply();
-        require(supply + mintAmount <= maxSupply, "Exceeds Max Supply");
-        require(msg.value >= costNova * mintAmount, "Insufficient Funds");
+        require (supply + mintAmount <= maxSupply, "Exceeds Max Supply");
+        require (msg.value >= costNova * mintAmount, "Insufficient Funds");
 
-        setBaseURI(
-            "https://gateway.pinata.cloud/ipfs/QmZnbPYvzatXfUbMk1XUUA3UqFqkomqBq1BmuELwgPzJTh/"
-        );
+        setBaseURI("https://gateway.pinata.cloud/ipfs/QmZnbPYvzatXfUbMk1XUUA3UqFqkomqBq1BmuELwgPzJTh/");
 
         if (msg.sender != owner()) {
             if (onlyWhitelisted == true) {
-                require(isNova(msg.sender), "User Is Not Whitelisted");
+                require (whitelistNova[msg.sender], "User Is Not Whitelisted");
+                require (totalMintNova[msg.sender] < amountNova[msg.sender], "Exceeded Whitelisted Amount!");
             }
         }
         uint256 tokenId = 9;
         totalNova += mintAmount;
+        totalMintNova[msg.sender] += mintAmount;
         _safeMint(msg.sender, tokenId + totalNova);
-        delete whitelistNova;
     }
 
     function mintAurora(uint256 mintAmount) public payable {
         uint256 supply = totalSupply();
-        require(supply + mintAmount <= maxSupply, "Exceeds Max Supply");
-        require(msg.value >= costAurora * mintAmount, "Insufficient Funds");
+        require (supply + mintAmount <= maxSupply, "Exceeds Max Supply");
+        require (msg.value >= costAurora * mintAmount, "Insufficient Funds");
 
-        setBaseURI(
-            "https://gateway.pinata.cloud/ipfs/QmNc1XfEf6UNfXRxa5hAsNmCsk1UUk2DNQp3CUtcyqDssf/"
-        );
+        setBaseURI("https://gateway.pinata.cloud/ipfs/QmNc1XfEf6UNfXRxa5hAsNmCsk1UUk2DNQp3CUtcyqDssf/");
 
         if (msg.sender != owner()) {
             if (onlyWhitelisted == true) {
-                require(isAurora(msg.sender), "User Is Not Whitelisted");
+                require (whitelistAurora[msg.sender], "User Is Not Whitelisted");
+                require (totalMintAurora[msg.sender] < amountAurora[msg.sender], "Exceeded Whitelisted Amount!");
             }
         }
         uint256 tokenId = 99;
         totalAurora += mintAmount;
+        totalMintAurora[msg.sender] += mintAmount;
         _safeMint(msg.sender, tokenId + totalAurora);
-        delete whitelistAurora;
     }
 
     function mintGenesis(uint256 mintAmount) public payable {
         uint256 supply = totalSupply();
-        require(supply + mintAmount <= maxSupply, "Exceeds Max Supply");
-        require(msg.value >= costGenesis * mintAmount, "Insufficient Funds");
+        require (supply + mintAmount <= maxSupply, "Exceeds Max Supply");
+        require (msg.value >= costGenesis * mintAmount, "Insufficient Funds");
 
-        setBaseURI(
-            "https://gateway.pinata.cloud/ipfs/QmZFjowfLsEX5bRbRgv9ojvKCgyTjoqRB6Qk9xqV74v8Bi/"
-        );
+        setBaseURI("https://gateway.pinata.cloud/ipfs/QmZFjowfLsEX5bRbRgv9ojvKCgyTjoqRB6Qk9xqV74v8Bi/");
 
         if (msg.sender != owner()) {
             if (onlyWhitelisted == true) {
-                require(isGenesis(msg.sender), "User Is Not Whitelisted");
+                require (whitelistGenesis[msg.sender], "User Is Not Whitelisted");
+                require (totalMintGenesis[msg.sender] < amountGenesis[msg.sender], "Exceeded Whitelisted Amount!");
             }
         }
         uint256 tokenId = 449;
         totalGenesis += mintAmount;
+        totalMintGenesis[msg.sender] += mintAmount;
         _safeMint(msg.sender, tokenId + totalGenesis);
-        delete whitelistGenesis;
     }
 
-    function isSuperNova(address _user) public view returns (bool) {
-        for (uint256 i = 0; i < whitelistSuperNova.length; i++) {
-            if (whitelistSuperNova[i] == _user) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function isNova(address _user) public view returns (bool) {
-        for (uint256 i = 0; i < whitelistNova.length; i++) {
-            if (whitelistNova[i] == _user) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function isAurora(address _user) public view returns (bool) {
-        for (uint256 i = 0; i < whitelistAurora.length; i++) {
-            if (whitelistAurora[i] == _user) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function isGenesis(address _user) public view returns (bool) {
-        for (uint256 i = 0; i < whitelistGenesis.length; i++) {
-            if (whitelistGenesis[i] == _user) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal whenNotPaused override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
@@ -179,51 +137,24 @@ contract WagmiClub is
         baseURI = _newBaseURI;
     }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        require(
-            _exists(tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        require(_exists(tokenId),"ERC721Metadata: URI query for nonexistent token");
         string memory currentBaseURI = _baseURI();
         return
-            bytes(currentBaseURI).length > 0
-                ? string(
-                    abi.encodePacked(
-                        currentBaseURI,
-                        Strings.toString(tokenId),
-                        baseExtension
-                    )
-                )
+        bytes(currentBaseURI).length > 0
+            ? string(abi.encodePacked(currentBaseURI, Strings.toString(tokenId), baseExtension))
                 : "";
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function walletOfOwner(address _owner)
-        public
-        view
-        returns (uint256[] memory)
-    {
+    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
         uint256 ownerTokenCount = balanceOf(_owner);
         uint256[] memory tokenIds = new uint256[](ownerTokenCount);
         for (uint256 i; i < ownerTokenCount; i++) {
@@ -243,24 +174,40 @@ contract WagmiClub is
         onlyWhitelisted = _state;
     }
 
-    function goSuperNova(address[] calldata _users) public onlyOwner {
-        delete whitelistSuperNova;
-        whitelistSuperNova = _users;
+    function goSuperNova (address[] memory _users, uint256 allowedAmount) public onlyOwner {
+        for (uint i = 0; i < _users.length; i++) {
+            if (_users[i] != address(0) ) {
+                whitelistSuperNova[_users[i]] = true;
+                amountSuperNova[_users[i]] += allowedAmount;
+            }   
+        }    
     }
 
-    function goNova(address[] calldata _users) public onlyOwner {
-        delete whitelistNova;
-        whitelistNova = _users;
+    function goNova (address[] memory _users, uint256 allowedAmount) public onlyOwner {
+        for (uint i = 0; i < _users.length; i++) {
+            if (_users[i] != address(0) ) {
+                whitelistNova[_users[i]] = true;
+                amountNova[_users[i]] += allowedAmount;
+            }   
+        }    
     }
 
-    function goAurora(address[] calldata _users) public onlyOwner {
-        delete whitelistAurora;
-        whitelistAurora = _users;
+    function goAurora (address[] memory _users, uint256 allowedAmount) public onlyOwner {
+        for (uint i = 0; i < _users.length; i++) {
+            if (_users[i] != address(0) ) {
+                whitelistAurora[_users[i]] = true;
+                amountAurora[_users[i]] += allowedAmount;
+            }   
+        }    
     }
 
-    function goGenesis(address[] calldata _users) public onlyOwner {
-        delete whitelistGenesis;
-        whitelistGenesis = _users;
+    function goGenesis (address[] memory _users, uint256 allowedAmount) public onlyOwner {
+        for (uint i = 0; i < _users.length; i++) {
+            if (_users[i] != address(0) ) {
+                whitelistGenesis[_users[i]] = true;
+                amountGenesis[_users[i]] += allowedAmount;
+            }   
+        }    
     }
 
     function setCostSuperNova(uint256 _newCostSuperNova) public onlyOwner {
