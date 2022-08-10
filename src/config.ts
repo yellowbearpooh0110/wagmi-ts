c66bc5a0fe82c7e826257e2d031bcfc20251365b
YellowBearPooh <yellowbearpooh0110@gmail.com>
2022-08-10 21:53:04 +0900

    mainnet update

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,9 @@
-export const contractAddress = '0xc79B574Dc5Ecc008799cAE0467f8a4116b59E09e';
+// export const contractAddress = '0xc79B574Dc5Ecc008799cAE0467f8a4116b59E09e';
 
-export const networkId = 4;
-export const networkIdHex = '0x4';
+// export const networkId = 4;
+// export const networkIdHex = '0x4';
+
+export const contractAddress = '0x88ddc3b4E6775c0A39b0E80Fc0a174d7a38397eC';
+
+export const networkId = 1;
+export const networkIdHex = '0x1';
